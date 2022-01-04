959eb8a4c0fc1e1333a0d0f983f1cef565a687c4
vinimadd <vinimadd@gmail.com>
2022-01-04 22:01:42 +0100

    add starting files

---
+++ src/main/java/com/vinimadd/interntask/InterntaskApplication.java
@@ -0,0 +1,13 @@
+package com.vinimadd.interntask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterntaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterntaskApplication.class, args);
+	}
+
+}
