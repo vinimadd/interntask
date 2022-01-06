6951627ee305d33bd4fa1220ea396068da0f28b2
vinimadd <vinimadd@gmail.com>
2022-01-06 14:59:42 +0100

    add packages and test api for a user

--- src/main/java/com/vinimadd/interntask/InterntaskApplication.java
+++ src/main/java/com/vinimadd/interntask/InterntaskApplication.java
@@ -2,12 +2,20 @@ package com.vinimadd.interntask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InterntaskApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InterntaskApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
